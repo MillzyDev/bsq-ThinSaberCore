0945abdb0678502c3a78ab02b5c584da50249fe5
Millzy <millzyyt08@gmail.com>
2023-02-15 14:22:38 +0000

    put in namespace

--- include/CustomTypes/Vector3.hpp
+++ include/CustomTypes/Vector3.hpp
@@ -1,7 +1,9 @@
 #include "beatsaber-hook/shared/utils/il2cpp-type-check.hpp"
 
-typedef struct Vector3 {
-    float x, y, z;
-} Vector3;
+namespace ThinSaberCore::CustomTypes {
+    typedef struct Vector3 {
+        float x, y, z;
+    } Vector3;
+}
 
-DEFINE_IL2CPP_ARG_TYPE(Vector3, "UnityEngine", "Vector3");
+DEFINE_IL2CPP_ARG_TYPE(ThinSaberCore::CustomTypes::Vector3, "UnityEngine", "Vector3");
