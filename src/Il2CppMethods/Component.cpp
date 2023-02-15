c576022edb5b6037dfbceeeef2af1b44bdf1a6ad
Millzy <millzyyt08@gmail.com>
2023-02-15 14:38:22 +0000

    use il2cpp_utils functions

--- src/Il2CppMethods/Component.cpp
+++ src/Il2CppMethods/Component.cpp
@@ -1,8 +1,8 @@
 #include "Il2CppMethods/Component.hpp"
-#include "Il2CppUtils.hpp"
 
 namespace ThinSaberCore::Il2CppMethods {
+
     Il2CppObject *Component::get_transform(Il2CppObject *obj) {
-        return Il2CppUtils::GetPropertyValue(obj, Il2CppUtils::GetProperty(obj, "transform"));
+        return CRASH_UNLESS(il2cpp_utils::GetPropertyValue(obj, "transform"));
     }
 }
