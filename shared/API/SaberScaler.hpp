55d142d02aeabe36be401f5d8e480eea297680a9
Millzy <millzyyt08@gmail.com>
2023-02-16 23:50:49 +0000

    add functions for legal lengths and scales

--- shared/API/SaberScaler.hpp
+++ shared/API/SaberScaler.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "CustomTypes/Vector3.hpp"
+
 namespace ThinSaberCore {
     class SaberScaler {
     public:
@@ -8,5 +10,14 @@ namespace ThinSaberCore {
 
         static float getSaberLength();
         static void setSaberLength(float value);
+
+        static float getLegalisedSaberThickness();
+        static float getLegalisedSaberLength();
+
+        static CustomTypes::Vector3 getSaberScale();
+        static CustomTypes::Vector3 getGlowScale();
+
+        static CustomTypes::Vector3 getLegalisedSaberScale();
+        static CustomTypes::Vector3 getLegalisedGlowScale();
     };
 }
--- src/API/SaberScaler.cpp
+++ src/API/SaberScaler.cpp
@@ -1,4 +1,5 @@
 #include "API/SaberScaler.hpp"
+#include "API/SaberJudge.hpp"
 #include "Config.hpp"
 
 namespace ThinSaberCore {
@@ -18,5 +19,37 @@ namespace ThinSaberCore {
     void SaberScaler::setSaberLength(float value) {
         Config::setLength(value);
         // TODO " "
+    }
+
+    float SaberScaler::getLegalisedSaberThickness() {
+        return SaberJudge::isThicknessLegal() ? getSaberThickness() : 1.0f;
+    }
+
+    float SaberScaler::getLegalisedSaberLength() {
+        return SaberJudge::isLengthLegal() ? getSaberLength() : 1.0f;;
+    }
+
+    CustomTypes::Vector3 SaberScaler::getSaberScale() {
+        float thickness = getSaberThickness();
+        float length = getSaberLength();
+        return { thickness, thickness, length};
+    }
+
+    CustomTypes::Vector3 SaberScaler::getGlowScale() {
+        float thickness = getSaberThickness();
+        float length = getSaberLength();
+        return { thickness, length, thickness};
+    }
+
+    CustomTypes::Vector3 SaberScaler::getLegalisedSaberScale() {
+        float thickness = getLegalisedSaberThickness();
+        float length = getLegalisedSaberLength();
+        return { thickness, thickness, length};
+    }
+
+    CustomTypes::Vector3 SaberScaler::getLegalisedGlowScale() {
+        float thickness = getLegalisedSaberThickness();
+        float length = getLegalisedSaberLength();
+        return { thickness, length, thickness};
     }
 }
