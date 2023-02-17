261fb414eab18a54698d01c86a8f17eea9066d36
Millzy <millzyyt08@gmail.com>
2023-02-17 14:56:46 +0000

    fix startup crash

--- src/Hooks/SaberModelController.cpp
+++ src/Hooks/SaberModelController.cpp
@@ -1,7 +1,7 @@
 #include "Hooks.hpp"
 #include "Il2CppMethods/Component.hpp"
 #include "Il2CppMethods/Transform.hpp"
-#include "API/SaberJudge.hpp"
+//#include "API/SaberJudge.hpp"
 #include "API/SaberScaler.hpp"
 
 using namespace ThinSaberCore::Il2CppMethods;
@@ -18,12 +18,15 @@ MAKE_HOOK_FIND_CLASS_UNSAFE_INSTANCE(SaberModelController_Init, "", "SaberModelC
     Il2CppObject *transform = Component::get_transform(self);
 
     Il2CppObject *basicSaberTransform = Transform::Find(transform, "BasicSaber");
+    if (!basicSaberTransform) return;
     Transform::set_localScale(basicSaberTransform, saberScale);
 
     Il2CppObject *fakeGlow0Transform = Transform::Find(transform, "FakeFlow0");
+    if (!fakeGlow0Transform) return;
     Transform::set_localScale(fakeGlow0Transform, glowScale);
 
     Il2CppObject *fakeFlow1Transform = Transform::Find(transform, "FakeFlow1");
+    if (!fakeFlow1Transform) return;
     Transform::set_localScale(fakeFlow1Transform, glowScale);
 }
 
