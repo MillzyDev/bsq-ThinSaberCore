50293bc834bae6dd89b4bff3b1c4d0686eda70de
Millzy <millzyyt08@gmail.com>
2023-03-03 18:39:42 +0000

    yeet law enforcement in header

--- shared/API/SaberJudge.hpp
+++ shared/API/SaberJudge.hpp
@@ -6,8 +6,5 @@ namespace ThinSaberCore {
         static bool isSaberScaleLegal();
         static bool isThicknessLegal();
         static bool isLengthLegal();
-
-        static bool getLawEnforcement();
-        static void setLawEnforcement(bool value);
     };
 }
